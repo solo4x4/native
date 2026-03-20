a2cd669dfdebd5136776b0018a920c745095510b
solo4x4 <62041606+solo4x4@users.noreply.github.com>
2026-03-20 02:15:53 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {StatusBar} from 'react-native';
 import SettingsScreen from './src/screens/SettingsScreen';
 import RemoteScreen from './src/screens/RemoteScreen';
